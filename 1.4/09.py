d5401ee1eb71c805f0993709dd91edb1c605c5f9
Павел Пацей <patsey.pavel@gmail.com>
2023-04-21 23:48:57 +0300

    solve 1.4.09

--- 1.4/09.py
+++ 1.4/09.py
@@ -1,5 +1,6 @@
 import sys
 
+# sys.stdin = open("input_09", "r", encoding="utf-8")
 # считывание списка строк из входного потока
 lst_in = list(map(str.strip, sys.stdin.readlines()))
 
@@ -15,11 +16,14 @@ class DataBase:
         (включительно) по их индексам (не id, а индексам списка); также учесть,
         что граница b может превышать длину списка.
         """
+        return self.lst_data[a: b + 1]
 
     def insert(self, data):
         """
         Добавляет в список lst_data новых данных из переданного списка строк data.
         """
+        for record in data:
+            self.lst_data.append(dict(zip(self.FIELDS, record.split())))
 
 
 db = DataBase()
