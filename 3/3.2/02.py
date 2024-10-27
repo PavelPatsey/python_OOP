4338cd86bc715882fb5e63d92a1a023c8a77c725
Павел <patsey.pavel@gmail.com>
2024-10-27 15:37:24 +0300

    solved 3.1.02

---
+++ 3/3.2/02.py
@@ -0,0 +1,23 @@
+from random import choice, randint
+
+
+class RandomPassword:
+    def __init__(self, psw_chars, min_length, max_length):
+        self.psw_chars = psw_chars
+        self.min_length = min_length
+        self.max_length = max_length
+
+    def __call__(self, *args, **kwargs):
+        password_length = randint(self.min_length, self.max_length)
+        password = ""
+        for _ in range(password_length):
+            password += choice(self.psw_chars)
+        return password
+
+
+min_length = 5
+max_length = 20
+psw_chars = "qwertyuiopasdfghjklzxcvbnm0123456789!@#$%&*"
+rnd = RandomPassword(psw_chars, min_length, max_length)
+
+lst_pass = [rnd() for _ in range(3)]
